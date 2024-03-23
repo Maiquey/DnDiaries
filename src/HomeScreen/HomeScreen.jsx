0fd4c88bae0c6c5295b3eb6601fcc8d0b23c3f86
nmf3 <nmf3@sfu.ca>
2024-03-23 14:53:34 -0700

    frontend progress

--- src/HomeScreen/HomeScreen.jsx
+++ src/HomeScreen/HomeScreen.jsx
@@ -1,22 +1,55 @@
-import React from "react";
-import './HomeScreen.css';
+    import React from "react";
+    import './HomeScreen.css';
+    import bgImage from '../Assets/openbook.jpg'
+    import bmIcon from '../Assets/icons8-bookmark-192-removebg-preview.png'
+    import inkIcon from '../Assets/icons8-ink-50.png'
 
-export default class HomeScreen extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            //state variables go here
-        };
+    export default class HomeScreen extends React.Component{
+        constructor(props){
+            super(props);
+            this.state = {
+                //state variables go here
+                // journalEntries: Array.from({length: 5}, () => '') // 5 empty strings
+            };
+        }
+
+        // handleJournalEntryChange = (index, event) => {
+        //     const { journalEntries } = this.state;
+        //     const updatedEntries = [...journalEntries];
+        //     updatedEntries[index] = event.target.value;
+        //     this.setState({ journalEntries: updatedEntries });
+        // }
+
+        handleClick() { 
+            // this.setState(prevState => ({
+            //     isToggleOn: !prevState.isToggleOn    
+            // }));  
+        }
+        
+        render(){
+            const { journalEntries } = this.state; // state vars here, make array of journal entries.
+
+            return (
+                <div className="bg" style={{ backgroundImage: `url(${bgImage})`, paddingTop: '50px' }}>
+                    {/* <div className="journal-entries">
+                        {journalEntries.map((entry, index) => (
+                            <div key={index} className="journal-entry">
+                                <h3>Journal Entry {index + 1}</h3>
+                                <textarea
+                                    value={entry}
+                                    onChange={(event) => this.handleJournalEntryChange(index, event)}
+                                    placeholder={`Journal entry ${index + 1}`}
+                                    rows={10}
+                                    cols={50}
+                                />
+                            </div>
+                        ))}
+                    </div> */}
+                    <button className="bookmark" onClick={ this.handleClick } style={{ backgroundImage: `url(${bmIcon})` }}>
+                    </button>
+                    <button className="newEntryButton" onClick={ this.handleClick } style={{ backgroundImage: `url(${inkIcon})` }}>
+                    </button>
+                </div>
+            );
+        }
     }
-
-    render(){
-        const {array} = this.state;
-
-        return(
-
-            <div className="helloMsg">
-                <button onClick={() => console.log("hello!")}>hello world!</button>
-            </div>
-        );
-    }
-}
