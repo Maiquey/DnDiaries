db837f3fdd14ec2b11db0615f667b0f65f6b144d
nmf3 <nmf3@sfu.ca>
2024-03-23 16:33:46 -0700

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ImageCreation from './ImageCreation/ImageCreation';
-
+import HomeScreen from './HomeScreen/HomeScreen';
 function App() {
   return (
     <div className="App">
-      <ImageCreation />
+      {/* <ImageCreation /> */}
+      <HomeScreen />
     </div>
   );
 }
--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 // import HomeScreen from './HomeScreen/HomeScreen.jsx';
 import './Modal.css';
+import dice from '../../Assets/d20.png'
 export default function Modal({ isOpen, toggleModal, contentType }) {
   return (
     <>
@@ -10,16 +11,14 @@ export default function Modal({ isOpen, toggleModal, contentType }) {
           <div className="modal-content">
             {contentType === 'bookmark' && (
               <>
+                <button className="addPC">
+                  <img src={dice} alt="Dice Image" />
+                  <span className="overlay-text">NEW</span>
+                </button>
                 <h2>Bookmark Modal</h2>
                 <p>This is the content for the bookmark modal.</p>
               </>
             )}
-            {/* {contentType === 'inkIcon' && (
-              <>
-                <h2>Ink Icon Modal</h2>
-                <p>This is the content for the ink icon modal.</p>
-              </>
-            )} */}
             <button className="close-modal" onClick={toggleModal}> CLOSE </button>
           </div>
         </div>
