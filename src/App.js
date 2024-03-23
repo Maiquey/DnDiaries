f61afd5bc7ecfb497c3fdbdb2fd2050ee460cfd9
Jan Mazurek <janofstargard@gmail.com>
2024-03-23 15:34:23 -0700

    implement basic page navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import HomeScreen from './HomeScreen/HomeScreen';
-import './App.css';
+import "./App.css";
+// importing components from react-router-dom package
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+ 
+import HomeScreen from "./Screens/HomeScreen";
+import CharacterScreen from "./Screens/CharacterCreation";
 
 function App() {
   return (
-    <div className="App">
-      <HomeScreen></HomeScreen>
-    </div>
+      <>
+          <Router>
+              <Routes>
+                  <Route
+                      exact
+                      path="/"
+                      element={<HomeScreen />}
+                  />
+                  <Route
+                      path="/character"
+                      element={<CharacterScreen />}
+                  />
+                  <Route
+                      path="*"
+                      element={<Navigate to="/" />}
+                  />
+              </Routes>
+          </Router>
+      </>
   );
 }
 
---
+++ src/Screens/HomeScreen.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import './HomeScreen.css';
+import { Link } from "react-router-dom";
+
+export default class HomeScreen extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            //state variables go here
+        };
+    }
+
+    render(){
+        const {array} = this.state;
+
+        return (
+            <div>
+                <h1>Home Page</h1>
+                <br />
+                <div className="helloMsg">
+                    <button onClick={() => console.log("hello from home screen")}>Welcome to home screen!</button>
+                </div>
+                <Link to="/">home</Link>
+                <br />
+                <Link to="/character">char</Link>
+                {/* <ul>
+                    <button>
+                    <li>
+                        <Link to="/"></Link>
+                    </li>
+                    </button>
+                    <br />
+                    <button>
+                    <li>
+                        <Link to="/character"></Link>
+                    </li>
+                    </button>
+                </ul> */}
+            </div>
+        );
+    }
+}
