55ca731f8fdef7643d71cba135591d38e79e38b0
Josh Chung <jca472@sfu.ca>
2024-03-23 16:42:46 -0700

    Save data with LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ImageCreation from './ImageCreation/ImageCreation';
+import RetrieveData from './RetrieveData/RetrieveData'
 
 function App() {
   return (
     <div className="App">
       <ImageCreation />
+      <RetrieveData />
     </div>
   );
 }
