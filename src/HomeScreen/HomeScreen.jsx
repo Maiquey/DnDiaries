d27aa1972ef5f877a814932dcef01ea752428918
nmf3 <nmf3@sfu.ca>
2024-03-23 15:49:23 -0700

    Added mainscreen UI

--- src/HomeScreen/HomeScreen.jsx
+++ src/HomeScreen/HomeScreen.jsx
@@ -1,53 +1,41 @@
     import React from "react";
+    import Modal from '../Components/Modal/Modal.js'
     import './HomeScreen.css';
     import bgImage from '../Assets/openbook.jpg'
     import bmIcon from '../Assets/icons8-bookmark-192-removebg-preview.png'
     import inkIcon from '../Assets/icons8-ink-50.png'
 
-    export default class HomeScreen extends React.Component{
-        constructor(props){
+    export default class HomeScreen extends React.Component {
+        constructor(props) {
             super(props);
             this.state = {
-                //state variables go here
-                // journalEntries: Array.from({length: 5}, () => '') // 5 empty strings
+                isModalOpen: false,
+                modalContentType: '', // Add state to control modal content type
             };
         }
-
-        // handleJournalEntryChange = (index, event) => {
-        //     const { journalEntries } = this.state;
-        //     const updatedEntries = [...journalEntries];
-        //     updatedEntries[index] = event.target.value;
-        //     this.setState({ journalEntries: updatedEntries });
-        // }
-
-        handleClick() { 
-            // this.setState(prevState => ({
-            //     isToggleOn: !prevState.isToggleOn    
-            // }));  
+    
+        openModalWithContent = (contentType) => { // Function to open modal with specific content
+            this.setState({
+                isModalOpen: true,
+                modalContentType: contentType,
+            });
         }
-        
-        render(){
-            const { journalEntries } = this.state; // state vars here, make array of journal entries.
-
+    
+        toggleModal = () => {
+            this.setState(prevState => ({
+                isModalOpen: !prevState.isModalOpen
+            }));
+        }
+    
+        render() {
+            const { isModalOpen, modalContentType } = this.state;
+    
             return (
                 <div className="bg" style={{ backgroundImage: `url(${bgImage})`, paddingTop: '50px' }}>
-                    {/* <div className="journal-entries">
-                        {journalEntries.map((entry, index) => (
-                            <div key={index} className="journal-entry">
-                                <h3>Journal Entry {index + 1}</h3>
-                                <textarea
-                                    value={entry}
-                                    onChange={(event) => this.handleJournalEntryChange(index, event)}
-                                    placeholder={`Journal entry ${index + 1}`}
-                                    rows={10}
-                                    cols={50}
-                                />
-                            </div>
-                        ))}
-                    </div> */}
-                    <button className="bookmark" onClick={ this.handleClick } style={{ backgroundImage: `url(${bmIcon})` }}>
+                    <Modal isOpen={isModalOpen} toggleModal={this.toggleModal} contentType={modalContentType}></Modal>
+                    <button className="bookmark" onClick={() => this.openModalWithContent('bookmark')} style={{ backgroundImage: `url(${bmIcon})` }}>
                     </button>
-                    <button className="newEntryButton" onClick={ this.handleClick } style={{ backgroundImage: `url(${inkIcon})` }}>
+                    <button className="newEntryButton" style={{ backgroundImage: `url(${inkIcon})` }}>
                     </button>
                 </div>
             );
